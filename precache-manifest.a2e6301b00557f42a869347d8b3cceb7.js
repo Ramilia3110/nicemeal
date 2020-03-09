8c5a6e6c8693a189be12a519a89cc9a3038e0f19
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2020-03-10 05:39:24 +0600

    Updates

---
+++ precache-manifest.a2e6301b00557f42a869347d8b3cceb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2fe8da5d8a951fd44c05349ca8133a",
+    "url": "/nicemeal/index.html"
+  },
+  {
+    "revision": "df60cca1084b04a80131",
+    "url": "/nicemeal/static/css/main.ef026e44.chunk.css"
+  },
+  {
+    "revision": "d6e2f8fd473f8fbdb6a5",
+    "url": "/nicemeal/static/js/2.47705147.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/nicemeal/static/js/2.47705147.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df60cca1084b04a80131",
+    "url": "/nicemeal/static/js/main.ea735141.chunk.js"
+  },
+  {
+    "revision": "f857ae1ccddd942a76f8",
+    "url": "/nicemeal/static/js/runtime-main.6ece5a0c.js"
+  },
+  {
+    "revision": "6a5b427c7eb85e787b939f39f8a519ac",
+    "url": "/nicemeal/static/media/chef.6a5b427c.jpg"
+  },
+  {
+    "revision": "38d13b5563e53d4330570c2281a08734",
+    "url": "/nicemeal/static/media/header.38d13b55.jpg"
+  },
+  {
+    "revision": "b04db001f458e19058c88a6863ee4a48",
+    "url": "/nicemeal/static/media/p1.b04db001.jpg"
+  },
+  {
+    "revision": "f2f493ece24379d382e1e81294a00b07",
+    "url": "/nicemeal/static/media/p2.f2f493ec.jpg"
+  },
+  {
+    "revision": "4f54984700eee49332fe04dde3057292",
+    "url": "/nicemeal/static/media/p3.4f549847.jpg"
+  }
+]);
